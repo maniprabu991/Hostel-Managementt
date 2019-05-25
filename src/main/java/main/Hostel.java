6c9534cbf8af5a503dc3a7a760f1223cc2c5ee72
acer <acer@acer-PC>
2019-05-25 15:40:05 +0530

    completed successfully

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -36,9 +36,14 @@ public class Hostel{
 	               String line = null;
 	            
 	               //write your code here !!!
-	               while ((line = br.readLine()) != null) {
+	               while ((line = br.readLine()) != null)
+	               {
 		               String[] splited = line.split("\\s+");
-		               String checkName = splited[0];
+		               String checkName = name;
+		               if(splited[0].equals(name))
+		               {
+		            	   return true;
+		               }
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
 	               }
@@ -47,11 +52,18 @@ public class Hostel{
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try
+      		{	
+      			writedata();
+      		}
+      			catch(IOException e)
+      			{
+      	            System.out.println(e);    
+      	        }
        }
 
        public static boolean verifyStudent(int regNo){
@@ -76,7 +88,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
-    	   
+    	   chk = readData(name);
     	   return chk;
         }
         
